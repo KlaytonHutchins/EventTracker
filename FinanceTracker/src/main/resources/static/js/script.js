152c8309ab2ce9475fe121368fe331859e214f8d
Klayton Hutchins <klaytonhutchins@gmail.com>
2022-11-13 20:38:36 -0500

    add detail header and back button. fix toFixed method on null bug

--- FinanceTracker/src/main/resources/static/js/script.js
+++ FinanceTracker/src/main/resources/static/js/script.js
@@ -1,12 +1,10 @@
 window.addEventListener('load', function() {
-	console.log('script loaded');
 	init();
 });
 
 function init() {
-	// setup event listeners
-	// load inital value
 	document.getElementById('detailDiv').style.display = 'none';
+	document.getElementById('detailHeader').style.display = 'none';
 	loadBankAccountList();
 	loadCreditCardList();
 	document.getElementsByName('newBankAcct')[0].addBankAcct.addEventListener('click', function(event) {
@@ -15,6 +13,20 @@ function init() {
 		if (bankAcct.institutionName !== '') {
 			createBankAcct(bankAcct);
 		}
+	})
+	document.getElementsByName('newCreditCard')[0].addCreditCard.addEventListener('click', function(event) {
+		event.preventDefault();
+		let creditCard = {
+			institutionName: document.getElementsByName('newCreditCard')[0].creditCard.value,
+			creditLimit: document.getElementsByName('newCreditCard')[0].creditLimit.value
+			}
+		if (creditCard.institutionName !== '' && creditCard.creditLimit !== '' && !isNaN(creditCard.creditLimit)) {
+			createCreditCard(creditCard);
+		}
+	})
+	document.getElementsByName('back')[0].backButton.addEventListener('click', function(event) {
+		event.preventDefault();
+		init();
 	})
 };
 
@@ -94,6 +106,44 @@ function loadWithdrawals(bankAcctId) {
 	xhr.send();
 };
 
+function loadPayments(creditCardId) {
+	let xhr = new XMLHttpRequest();
+	xhr.open('GET', 'api/portfolios/1/creditCards/'+creditCardId+'/payments', true);
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4) {
+			if (xhr.status === 200) {
+				let payments = JSON.parse(xhr.responseText);
+				displayPayments(creditCardId, payments);
+				displayError('');
+			} else if (xhr.status === 404) {
+				displayError('Payments Not Found');
+			} else {
+				displayError('An error occured: ' + xhr.status);
+			}
+		}
+	};
+	xhr.send();
+};
+
+function loadPurchases(creditCardId) {
+	let xhr = new XMLHttpRequest();
+	xhr.open('GET', 'api/portfolios/1/creditCards/'+creditCardId+'/purchases', true);
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4) {
+			if (xhr.status === 200) {
+				let purchases = JSON.parse(xhr.responseText);
+				displayPurchases(creditCardId, purchases);
+				displayError('');
+			} else if (xhr.status === 404) {
+				displayError('Purchases Not Found');
+			} else {
+				displayError('An error occured: ' + xhr.status);
+			}
+		}
+	};
+	xhr.send();
+};
+
 function displayError(msg) {
 	let errorDiv = document.getElementById('error');
 	errorDiv.textContent = '';
@@ -111,11 +161,17 @@ function displayBankAccts(bankAccts) {
 			td.textContent = bankAcct.institutionName;
 			tr.appendChild(td);
 			td = document.createElement('td');
-			td.textContent = bankAcct.balance;
+			if (bankAcct.balance !== null) {
+				td.textContent = bankAcct.balance.toFixed(2);
+			} else {
+				td.textContent = 0;
+			}
 			tr.appendChild(td);
 			bankAcctsBody.appendChild(tr);
 			tr.addEventListener('click', function(event) {
 				event.preventDefault();
+				let detailHeader = document.getElementById('detailH2');
+				detailHeader.textContent = 'Bank Account - '+bankAcct.institutionName;
 				loadDeposits(bankAcct.id);
 				loadWithdrawals(bankAcct.id);
 			});
@@ -128,25 +184,30 @@ function displaycreditCards(creditCards) {
 	let creditCardsBody = document.getElementById('creditCardsBody');
 	creditCardsBody.textContent = '';
 	if (creditCards && Array.isArray(creditCards) && creditCards.length > 0) {
+	console.log(creditCards);
 		for (let creditCard of creditCards) {
+			console.log('in loop');
 			let tr = document.createElement('tr');
 			let td = document.createElement('td');
-			
 			td.textContent = creditCard.institutionName;
 			tr.appendChild(td);
-			
 			td = document.createElement('td');
-			td.textContent = creditCard.balance;
+			if (creditCard.balance !== null) {
+				td.textContent = creditCard.balance.toFixed(2);
+			} else {
+				td.textContent = 0;
+			}
 			tr.appendChild(td);
-			
 			td = document.createElement('td');
 			td.textContent = creditCard.creditLimit;
 			tr.appendChild(td);
-			
 			creditCardsBody.appendChild(tr);
 			tr.addEventListener('click', function(event) {
 				event.preventDefault();
-				// individual bank account view
+				let detailHeader = document.getElementById('detailH2');
+				detailHeader.textContent = 'Credit Card - '+creditCard.institutionName+' (Limit: '+creditCard.creditLimit+')';
+				loadPayments(creditCard.id);
+				loadPurchases(creditCard.id);
 			});
 		}
 	}
@@ -156,6 +217,7 @@ function displayDeposits(bankAcctId, deposits) {
 	document.getElementById('bankAcctDiv').style.display = 'none';
 	document.getElementById('creditCardDiv').style.display = 'none';
 	document.getElementById('detailDiv').style.display = 'flex';
+	document.getElementById('detailHeader').style.display = 'flex';
 	let depositPaymentHeader = document.getElementById('depositPaymentHeader');
 	depositPaymentHeader.textContent = 'Deposits';
 	let depositPaymentButton = document.getElementsByName('depositPaymentButton')[0];
@@ -169,7 +231,6 @@ function displayDeposits(bankAcctId, deposits) {
 		if (!isNaN(deposit.amount) && deposit.amount !== '' && deposit.description !== '') {
 			document.getElementsByName('depositPaymentAmount')[0].value = '';
 			document.getElementsByName('depositPaymentDescription')[0].value = '';
-			console.log(deposit);
 			createDeposit(bankAcctId, deposit);
 		}
 	});
@@ -180,7 +241,7 @@ function displayDeposits(bankAcctId, deposits) {
 			let tr = document.createElement('tr');
 			let td = document.createElement('td');
 			
-			td.textContent = deposit.amount;
+			td.textContent = deposit.amount.toFixed(2);
 			tr.appendChild(td);
 			
 			td = document.createElement('td');
@@ -200,6 +261,7 @@ function displayWithdrawals(bankAcctId, withdrawals) {
 	document.getElementById('bankAcctDiv').style.display = 'none';
 	document.getElementById('creditCardDiv').style.display = 'none';
 	document.getElementById('detailDiv').style.display = 'flex';
+	document.getElementById('detailHeader').style.display = 'flex';
 	let withdrawalPurchaseHeader = document.getElementById('withdrawalPurchaseHeader');
 	withdrawalPurchaseHeader.textContent = 'Withdrawals';
 	let withdrawalPurchaseButton = document.getElementsByName('withdrawalPurchaseButton')[0];
@@ -223,7 +285,7 @@ function displayWithdrawals(bankAcctId, withdrawals) {
 			let tr = document.createElement('tr');
 			let td = document.createElement('td');
 			
-			td.textContent = withdrawal.amount;
+			td.textContent = withdrawal.amount.toFixed(2);
 			tr.appendChild(td);
 			
 			td = document.createElement('td');
@@ -235,6 +297,94 @@ function displayWithdrawals(bankAcctId, withdrawals) {
 			tr.appendChild(td);
 			
 			withdrawalBody.appendChild(tr);
+		}
+	}
+};
+
+function displayPayments(creditCardId, payments) {
+	document.getElementById('bankAcctDiv').style.display = 'none';
+	document.getElementById('creditCardDiv').style.display = 'none';
+	document.getElementById('detailDiv').style.display = 'flex';
+	document.getElementById('detailHeader').style.display = 'flex';
+	let depositPaymentHeader = document.getElementById('depositPaymentHeader');
+	depositPaymentHeader.textContent = 'Payments';
+	let depositPaymentButton = document.getElementsByName('depositPaymentButton')[0];
+	depositPaymentButton.textContent = 'Add Payment';
+	depositPaymentButton.addEventListener('click', function(event) {
+		event.preventDefault();
+		let payment = {
+			amount: document.getElementsByName('depositPaymentAmount')[0].value,
+			description: document.getElementsByName('depositPaymentDescription')[0].value
+		}
+		if (!isNaN(payment.amount) && payment.amount !== '' && payment.description !== '') {
+			document.getElementsByName('depositPaymentAmount')[0].value = '';
+			document.getElementsByName('depositPaymentDescription')[0].value = '';
+			createPayment(creditCardId, payment);
+		}
+	});
+	let paymentBody = document.getElementById('depositPaymentBody');
+	paymentBody.textContent = '';
+	if (payments && Array.isArray(payments) && payments.length > 0) {
+		for (let payment of payments) {
+			let tr = document.createElement('tr');
+			let td = document.createElement('td');
+			
+			td.textContent = payment.amount.toFixed(2);
+			tr.appendChild(td);
+			
+			td = document.createElement('td');
+			td.textContent = payment.timestamp;
+			tr.appendChild(td);
+			
+			td = document.createElement('td');
+			td.textContent = payment.description;
+			tr.appendChild(td);
+			
+			paymentBody.appendChild(tr);
+		}
+	}
+};
+
+function displayPurchases(creditCardId, purchases) {
+	document.getElementById('bankAcctDiv').style.display = 'none';
+	document.getElementById('creditCardDiv').style.display = 'none';
+	document.getElementById('detailDiv').style.display = 'flex';
+	document.getElementById('detailHeader').style.display = 'flex';
+	let withdrawalPurchaseHeader = document.getElementById('withdrawalPurchaseHeader');
+	withdrawalPurchaseHeader.textContent = 'Purchases';
+	let withdrawalPurchaseButton = document.getElementsByName('withdrawalPurchaseButton')[0];
+	withdrawalPurchaseButton.textContent = 'Add Purchase';
+	withdrawalPurchaseButton.addEventListener('click', function(event) {
+		event.preventDefault();
+		let purchase = {
+			amount: document.getElementsByName('withdrawalPurchaseAmount')[0].value,
+			description: document.getElementsByName('withdrawalPurchaseDescription')[0].value
+		}
+		if (!isNaN(purchase.amount) && purchase.amount !== '' && purchase.description !== '') {
+			document.getElementsByName('withdrawalPurchaseAmount')[0].value = '';
+			document.getElementsByName('withdrawalPurchaseDescription')[0].value = '';
+			createPurchase(creditCardId, purchase);
+		}
+	});
+	let purchaseBody = document.getElementById('withdrawalPurchaseBody');
+	purchaseBody.textContent = '';
+	if (purchases && Array.isArray(purchases) && purchases.length > 0) {
+		for (let purchase of purchases) {
+			let tr = document.createElement('tr');
+			let td = document.createElement('td');
+			
+			td.textContent = purchase.amount.toFixed(2);
+			tr.appendChild(td);
+			
+			td = document.createElement('td');
+			td.textContent = purchase.timestamp;
+			tr.appendChild(td);
+			
+			td = document.createElement('td');
+			td.textContent = purchase.description;
+			tr.appendChild(td);
+			
+			purchaseBody.appendChild(tr);
 		}
 	}
 };
@@ -291,5 +441,59 @@ function createWithdrawal(bankAcctId, withdrawal) {
 		}
 	};
 	xhr.send(JSON.stringify(withdrawal));
+};
+
+function createCreditCard(creditCard) {
+	let xhr = new XMLHttpRequest();
+	xhr.open('POST', 'api/portfolios/1/creditCards', true);
+	xhr.setRequestHeader("Content-type", "application/json");
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4) {
+			if (xhr.status === 201) {
+				creditCard = JSON.parse(xhr.responseText);
+				loadPayments(creditCard.id);
+				loadPurchases(creditCard.id);
+			} else {
+				displayError('An error occured: ' + xhr.status);
+			}
+		}
+	};
+	xhr.send(JSON.stringify(creditCard));
+};
+
+function createPayment(creditCardId, payment) {
+	let xhr = new XMLHttpRequest();
+	xhr.open('POST', 'api/portfolios/1/creditCards/'+creditCardId+'/payments', true);
+	xhr.setRequestHeader("Content-type", "application/json");
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4) {
+			if (xhr.status === 201) {
+				payment = JSON.parse(xhr.responseText);
+				loadPayments(creditCardId);
+				loadPurchases(creditCardId);
+			} else {
+				displayError('An error occured: ' + xhr.status);
+			}
+		}
+	};
+	xhr.send(JSON.stringify(payment));
+};
+
+function createPurchase(creditCardId, purchase) {
+	let xhr = new XMLHttpRequest();
+	xhr.open('POST', 'api/portfolios/1/creditCards/'+creditCardId+'/purchases', true);
+	xhr.setRequestHeader("Content-type", "application/json");
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4) {
+			if (xhr.status === 201) {
+				purchase = JSON.parse(xhr.responseText);
+				loadPayments(creditCardId);
+				loadPurchases(creditCardId);
+			} else {
+				displayError('An error occured: ' + xhr.status);
+			}
+		}
+	};
+	xhr.send(JSON.stringify(purchase));
 };
 
